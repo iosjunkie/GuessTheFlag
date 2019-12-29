9f4b71efef326891cf4cea1f0177e12d54b78662
Jules Lee <jackconsolidated@gmail.com>
2019-12-29 15:37:58 +0800

    Initial Commit

---
+++ DayOneSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DayOneSwiftUI
+//
+//  Created by Jules Lee on 12/29/19.
+//  Copyright © 2019 Jules Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
